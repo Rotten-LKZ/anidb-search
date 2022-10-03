deacae1515834a31a64037a0b9269f8067a71c01
Rotten-LKZ <Rotten_LKZ@163.com>
2022-10-03 23:30:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# anidb-search
+
+Anidb Searching API.（anidb 搜索 API）
+
+## 中文
+
+此 API 数据每天更新一次。
+
+
+
+## English
---
+++ script/hundle.ts
@@ -0,0 +1,90 @@
+import fs from 'fs/promises'
+import path from 'path'
+import xml2js from 'xml2js'
+import pinyin from 'pinyin'
+import { title } from 'process'
+
+type Return = {
+  [key: string]: ReturnAni[]
+}
+
+type ReturnAni = {
+  name: string
+  type: string
+  lang: string
+}
+
+type ParsedResult = {
+  animetitles: { anime: Anime[] }
+}
+
+type Anime = {
+  $: { aid: string }
+  title: Title[]
+}
+
+type Title = {
+  _: string
+  $: TitleAttr
+}
+
+type TitleAttr = {
+  type: string
+  'xml:lang': string
+}
+
+fs.readFile(path.resolve('./anime-titles.xml'), { encoding: 'utf-8' })
+  .then(data => {
+    xml2js.parseString(data, (err, res: ParsedResult) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+
+      const ret: Return = {}
+
+      for (let ani of res.animetitles.anime) {
+        const retAni: ReturnAni[] = []
+        for (let titles of ani.title) {
+          retAni.push({
+            name: titles._,
+            type: titles.$.type,
+            lang: titles.$['xml:lang'],
+          })
+          if (titles.$['xml:lang'].startsWith('zh')) {
+            // Add Pinyin
+            retAni.push({
+              name: pinyin(titles._, { style: 'normal' }).flat().join(''),
+              type: 'pinyin',
+              lang: titles.$['xml:lang'],
+            })
+
+            retAni.push({
+              name: pinyin(titles._, { style: 'normal' }).flat().join(' '),
+              type: 'pinyin-space',
+              lang: titles.$['xml:lang'],
+            })
+
+            retAni.push({
+              name: pinyin(titles._, { style: 'normal' }).flat().map(v => v[0]).join(''),
+              type: 'pinyin-initial',
+              lang: titles.$['xml:lang'],
+            })
+          }
+        }
+
+        ret[ani.$.aid] = retAni
+      }
+
+      fs.writeFile(path.resolve('./anime-titles.json'), JSON.stringify(ret, undefined, 2))
+        .then(() => {
+          console.log('Convert successfully.')
+        }).catch(err => {
+          console.error(err)
+          process.exit(1)
+        })
+    })
+  }).catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
